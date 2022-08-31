0a6d5d57e4b6483f0c561b4765fd780a9a119cef
Eve Aviv Keinan <evavkein@gmail.com>
2022-08-31 18:22:10 +0300

    refactor: simplify (a ton) using mutationObserver
    feels good to eliminate a ton of complexity and janky/unreliable behavior... but this also deleted some neat logging, which i'm eager to reintroduce as an actual element (because opening the console is an unnecessary chore, and the console is bothersome to parse when it's bloated by the site's logs)

--- script.js
+++ script.js
@@ -1,20 +1,10 @@
 let filteredString;
 chrome.storage.sync.get(["blacklisted"], (current) => filteredString = current.blacklisted);
 
-const waitPeriod = { // very arbitrary, based on rough testing with a particular device on a particular internet connection
-  scrollAllListings: 1000, 
-  fetchAndLoadListings: 1250,
-  coldVisit: 3000,
-};
+const waitForColdVisit = 2500; // very arbitrary, based on rough testing with a particular device on a particular internet connection
 
-const listingsPerPage = 25;
-
-function filterListings() {
+function filterNewListings() { // 
   const allListings = Array.from(document.querySelectorAll(".job-card-container"));
-  if (allListings.length !== listingsPerPage) {
-    console.log(`Failed to parse all listings! Found ${allListings.length}/${listingsPerPage}`);
-      // i'd log this as an error/warn, but chrome logs these as extension errors instead of writing to console
-  };
 
   const matchingListings = allListings.filter((listing) => {
     return listing.textContent.includes(filteredString);
@@ -26,28 +16,20 @@ function filterListings() {
     removedCount++; 
   });
 
-  console.log(
-    removedCount
-      ? `Removed ${removedCount} "${filteredString}" listing${removedCount > 1 ? "s" : ""}!`
-      : `Didn't find a single "${filteredString}" listing on this page :D`
+  if (removedCount) {
+    console.log(`Removed ${removedCount} "${filteredString}" listing${removedCount > 1 ? "s" : ""}!`)
+  };
+};
+
+function setup() {
+  const filterOnMutation = new MutationObserver(filterNewListings);
+  filterOnMutation.observe(
+    document.querySelector(".scaffold-layout__list-container"),
+    {childList: true, subtree: true}
   );
 };
 
-function filterAllListings(delayUntilLoaded) {
-  setTimeout(async () => {
-    document.querySelector("footer").scrollIntoView({behavior: "smooth"});
-      // used to have an intersectionObserver with a debounced disconnect to figure out when scrolling was done,
-      // but the debounce timer was a magic number and the scroll length doesn't vary much, so i deleted all that unnecessary complexity in favor of one overarching magic number.
-      // might switch to some version of the intersectionObserver solution in future, if/when i wanna make this *more* reliable and have a proper (i.e non-magic-number) solution for the debounce
-    setTimeout(() => {
-      filterListings();
-    }, waitPeriod.scrollAllListings);
-
-    const pageButtons = document.querySelectorAll(".jobs-search-results-list__pagination button");
-    pageButtons.forEach((pageButton) => {
-      pageButton.addEventListener("click", () => filterAllListings(waitPeriod.fetchAndLoadListings));
-    });
-  }, delayUntilLoaded);
-};
-
-filterAllListings(waitPeriod.coldVisit + waitPeriod.fetchAndLoadListings);
+setTimeout(() => {
+  setup()
+  filterNewListings();
+}, waitForColdVisit);
