9537567af318e72fa351e269b7788c8ae03c17e2
Eve Aviv Keinan <evavkein@gmail.com>
2022-08-31 20:38:02 +0300

    feat: in-page element with counter & filtered text

--- main.js
+++ main.js
@@ -3,25 +3,36 @@ chrome.storage.sync.get(["blacklisted"], (current) => filteredString = current.b
 
 const waitForColdVisit = 2500; // very arbitrary, based on rough testing with a particular device on a particular internet connection
 
-function filterNewListings() { // 
+const containerElem = document.createElement("aside");
+const counterElem = document.createElement("h6");
+const filtersElem = document.createElement("p");
+containerElem.id = "EAK_JobsFilter";
+containerElem.appendChild(counterElem);
+containerElem.appendChild(filtersElem);
+
+function updateCounterElem(newCount) {
+  counterElem.innerText = "Total jobs filtered: " + newCount;
+};
+
+let filterCount = 0;
+function filterNewListings() {
   const allListings = Array.from(document.querySelectorAll(".job-card-container"));
 
   const matchingListings = allListings.filter((listing) => {
     return listing.textContent.includes(filteredString);
   });
   
-  let removedCount = 0;
   matchingListings.forEach((matchingListing) => {
     matchingListing.remove();
-    removedCount++; 
+    updateCounterElem(filterCount++);
   });
-
-  if (removedCount) {
-    console.log(`Removed ${removedCount} "${filteredString}" listing${removedCount > 1 ? "s" : ""}!`)
-  };
 };
 
 function setup() {
+  updateCounterElem(0);
+  filtersElem.innerText = "Filtering: " + filteredString;
+  document.body.appendChild(containerElem);
+
   const filterOnMutation = new MutationObserver(filterNewListings);
   filterOnMutation.observe(
     document.querySelector(".scaffold-layout__list-container"),
