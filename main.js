75748a509fa6ae34cd17094250bf10c1e95c0243
Eve Aviv Keinan <evavkein@gmail.com>
2022-09-01 02:30:26 +0300

    feat: multiple filters

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-let filteredString;
-chrome.storage.sync.get(["blacklisted"], (current) => filteredString = current.blacklisted);
+let filterCount = 0;
+let filteredStrings = [];
 
 const selectors = {
   jobsList: ".scaffold-layout__list-container",
@@ -17,12 +17,25 @@ function updateCounterElem(newCount) {
   counterElem.innerText = "Total jobs filtered: " + newCount;
 };
 
-let filterCount = 0;
+async function getFilters() { // copy-pasted from settings.js, because using modules in chrome extensions requires some very wacky code. also, see that file for context about this function
+  return new Promise((resolve) => {
+    chrome.storage.sync.get(["filtered"], (storage) => resolve(storage.filtered));
+  });
+};
+
 function filterNewListings() {
   const allListings = Array.from(document.querySelectorAll(selectors.jobItem));
 
   const matchingListings = allListings.filter((listing) => {
-    return listing.textContent.includes(filteredString);
+    let matchesAnyFilter;
+
+    filteredStrings.forEach((string) => {
+      if (listing.textContent.includes(string)) {
+        matchesAnyFilter = true;
+      };
+    });
+
+    return matchesAnyFilter;
   });
   
   matchingListings.forEach((matchingListing) => {
@@ -33,7 +46,13 @@ function filterNewListings() {
 
 function setup() {
   updateCounterElem(0);
-  filtersElem.innerText = "Filtering: " + filteredString;
+
+  let filtersAsString = "";
+  for (const [index, filter] of filteredStrings.entries()) { // initially wanted to use Array.reduce(), because this is a (rare) perfect case for it, but it's just not readable enough
+    filtersAsString += (index ? ", " + filter : filter);
+  };
+
+  filtersElem.innerText = "Filtering: " + filtersAsString;
   document.body.appendChild(containerElem);
 
   const filterOnMutation = new MutationObserver(filterNewListings);
@@ -43,14 +62,18 @@ function setup() {
   );
 };
 
-const initOnceReady = new MutationObserver(() => {
-  if (document.querySelector(selectors.jobsList)) {
-    initOnceReady.disconnect();
-    setup();
-    filterNewListings();
-  };
+getFilters().then((filters) => {
+  filteredStrings = filters;
+
+  const initOnceReady = new MutationObserver(() => {
+    if (document.querySelector(selectors.jobsList)) {
+      initOnceReady.disconnect();
+      setup();
+      filterNewListings();
+    };
+  });
+  initOnceReady.observe(
+    document.body,
+    {childList: true, subtree: true}
+  );
 });
-initOnceReady.observe(
-  document.body,
-  {childList: true, subtree: true}
-);
--- settings.js
+++ settings.js
@@ -1,16 +1,53 @@
-const currentFilter = document.querySelector("span");
-const textInput = document.querySelector("input");
-const saveButton = document.querySelector("button");
+let filtered = [];
 
-function updateCurrentFilterElement() {
-  chrome.storage.sync.get(["blacklisted"], (current) => {
-    currentFilter.innerText = current.blacklisted;
+const elems = {
+  textInput: document.querySelector("input"),
+  saveButton: document.querySelector("button"),
+  filtersList: document.querySelector("ul"),
+};
+
+async function getFilters() { // i'd just chain a "then" after the storage's get function if it was async. according to the docs (as of august 2022), they're planning on eventually making it so 
+  return new Promise((resolve) => {
+    chrome.storage.sync.get(["filtered"], (storage) => resolve(storage.filtered));
   });
 };
 
-updateCurrentFilterElement();
+function newFilterListItem(filterText) {
+  const filterElem = document.createElement("li");
+  filterElem.innerText = filterText;
 
-saveButton.addEventListener("click", () => {
-  chrome.storage.sync.set({blacklisted: textInput.value});
-  updateCurrentFilterElement();
+  const deletionButton = document.createElement("button");
+  deletionButton.innerText = "Delete";
+  filterElem.appendChild(deletionButton);
+
+  deletionButton.addEventListener("click", function deleteFilter() {
+    const filteredAfterDeletion = filtered.filter((filter) => filter != filterText);
+
+    chrome.storage.sync.set({filtered: filteredAfterDeletion});
+    filtered = filteredAfterDeletion;
+
+    deletionButton.removeEventListener("click", deleteFilter);
+    filterElem.remove();
+  }, {once: true});
+  
+  elems.filtersList.appendChild(filterElem);
+};
+
+getFilters().then((filteredStrings) => {
+  if (!filteredStrings) chrome.storage.sync.set({filtered: []});
+  filtered = filteredStrings ?? [];
+  
+  filtered.forEach((filterText) => {
+    newFilterListItem(filterText);
+  });
+  
+  elems.saveButton.addEventListener("click", () => {
+    const newFilter = elems.textInput.value;
+    const filteredAfterAddition = [...filtered, newFilter];
+    
+    chrome.storage.sync.set({filtered: filteredAfterAddition});
+    filtered = filteredAfterAddition;
+    newFilterListItem(newFilter);
+    elems.textInput.value = "";
+  });
 });
