a0721647ce667d644fbb37219ac310f15689fbca
Eve Aviv Keinan <evavkein@gmail.com>
2022-09-05 15:13:35 +0300

    feat: support for multiple pages per site (starting with logged-out pages)

--- main.js
+++ main.js
@@ -1,14 +1,26 @@
-class SiteData {
-  constructor(siteName, jobsListSelector, jobsItemSelector, manualStart) {
-    this.name = siteName;
+class PageData {
+  constructor(jobsListSelector, jobItemSelector, manualStart) {
     this.jobsList = jobsListSelector;
-    this.jobItem = jobsItemSelector;
+    this.jobItem = jobItemSelector;
     this.manualStart = manualStart; // created due to AngelList throwing a 404 with a bunch of console errors when changing sort/filter options after the filtering mutationObserver is attached. this is not happening with LinkedIn. AngelList uses NextJS, and my best guess is that the incompatibility has something to do with nextJS's SPA mechanisms
   };
 };
+
+class SiteData {
+  constructor(siteName, pagesDataArray) {
+    this.name = siteName;
+    this.pages = pagesDataArray; // created due to both LinkedIn & AngelList (and likely more) having differently-attributed elements for logged-out and logged-out job browsing. didn't do explicit "logged in" & "logged out" URL-based setups because AngelList uses for the exact same URL for both pages
+  };
+};
 const compatibleSites = [
-  new SiteData("linkedin", ".scaffold-layout__list-container", ".job-card-container", false),
-  new SiteData("angel.co", '[data-test="JobSearchResults"]', '[data-test="StartupResult"]', true),
+  new SiteData("linkedin", [
+    new PageData(".jobs-search__results-list", ".job-search-card"),
+    new PageData(".scaffold-layout__list-container", ".job-card-container"),
+  ]),
+  new SiteData("angel.co", [
+    new PageData(".styles_results__ZQhDf", ".styles_result__rPRNG"), // this page just has some listings from a few popular companies before prompting the user to register, but i'm supporting it on principle. also, wont be surprised if these class suffixes end up changing when they recompile for an update
+    new PageData('[data-test="JobSearchResults"]', '[data-test="StartupResult"]', true),
+  ]),
 ];
 
 function findSiteData() {
@@ -18,6 +30,27 @@ function findSiteData() {
       data = siteData;
       break;
     };
+  };
+  return data;
+};
+
+function lookForPageData(siteData) {
+  let data;
+  for (const pageData of siteData.pages) {
+    if (document.querySelector(pageData.jobsList)) {
+      data = pageData;
+      break;
+    };
+  };
+  return data;
+};
+
+async function findPageData(siteData) { // because some pages' body loads slowly enough (e.g AngelList's logged-out jobs page) that checking page elements right as it loads will be premature
+  let data;
+  for (let i = 0; i < 3; i++) {
+    data = lookForPageData(siteData);
+    if (data) break; 
+    await new Promise(resolve => setTimeout(resolve, 250));
   };
   return data;
 };
@@ -62,10 +95,10 @@ function initialize() {
   containerElem.appendChild(descriptionElem);
   document.body.appendChild(containerElem);
 
-  const site = findSiteData();
-  if (!site) {
-    titleElem.innerText = "Error: Failed to load the specs for site";
-    descriptionElem.innerText = "Please report this error at https://github.com/EvAvKein/FilterJobsFeeds/issues/new (and include this page's URL)";
+  const siteData = findSiteData();
+  if (!siteData) {
+    titleElem.innerText = "Error: Failed to load specs for this site";
+    descriptionElem.innerText = "Please report this at https://github.com/EvAvKein/FilterJobsFeeds/issues/new (with this page's URL)";
     return;
   };
 
@@ -75,52 +108,66 @@ function initialize() {
       descriptionElem.innerText = "Edit your filters in the extension settings";
       return;
     };
-    descriptionElem.innerText = "Filtering: " + stringsArrayToList(filters);
-  
+    
     let totalFiltered = 0;
-    function filterJobs() {
-      totalFiltered += filterListings(site.jobItem, filters);
+    function filterJobs(pageData) {
+      totalFiltered += filterListings(pageData.jobItem, filters);
       titleElem.innerText = "Total jobs filtered: " + totalFiltered;
     };
 
-    const initOnceReady = new MutationObserver(() => {
-      if (document.querySelector(site.jobsList)) {
-        initOnceReady.disconnect();
+    console.log("initOnceReady")
+    const initOnceReady = new MutationObserver(async () => {
+      console.log("body ready!")
+      initOnceReady.disconnect();
 
-        const filterOnMutation = new MutationObserver(filterJobs);
-        function startFiltering() {
-          filterJobs();
-          filterOnMutation.observe(
-            document.querySelector(site.jobsList),
-            {childList: true, subtree: true}
-          );
-        };
+      titleElem.innerText = "Searching for site specs...";
+      descriptionElem.innerText = "";
 
-        if (!site.manualStart) {
-          startFiltering();
-          return;
-        };
-
-        const toggleSectionElem = document.createElement("section");
-        toggleSectionElem.id = "EAK_toggleSection";
-        toggleSectionElem.innerHTML = `
-          <hr>
-          <h6> Activate once you're ready to browse! </h6>
-          <p> Manual activation is required due to conflicts with search/sort functionalities,<br> so first make sure you're done editing those! </p>
-        `; // dont need references to any of these, so there's no reason to not use innerHTML. halves the complexity & lines of code
-
-        const toggleButtonElem = document.createElement("button");
-        toggleButtonElem.innerText = "ACTIVATE";
-        toggleSectionElem.appendChild(toggleButtonElem);
-
-        toggleButtonElem.addEventListener("click", function manualStartFiltering() {
-          startFiltering();
-          toggleButtonElem.removeEventListener("click", manualStartFiltering);
-          toggleSectionElem.remove();
-        });
-
-        containerElem.appendChild(toggleSectionElem);
+      const pageData = await findPageData(siteData);
+      if (!pageData) {
+        titleElem.innerText = "Error: Failed to load specs for this page";
+        descriptionElem.innerText = "Please report this at https://github.com/EvAvKein/FilterJobsFeeds/issues/new (with this page's URL and a screenshot)";
+        return;
       };
+
+      titleElem.innerText = "";
+      descriptionElem.innerText = "Filtering: " + stringsArrayToList(filters);
+
+      const filterOnMutation = new MutationObserver(() => {filterJobs(pageData)});
+      function startFiltering() {
+        console.log("started filtering")
+        filterJobs(pageData);
+        filterOnMutation.observe(
+          document.querySelector(pageData.jobsList),
+          {childList: true, subtree: true}
+        );
+      };
+
+      console.log("manual start?", pageData.manualStart)
+      if (!pageData.manualStart) {
+        startFiltering();
+        return;
+      };
+
+      const toggleSectionElem = document.createElement("section");
+      toggleSectionElem.id = "EAK_toggleSection";
+      toggleSectionElem.innerHTML = `
+        <hr>
+        <h6> Activate once you're ready to browse! </h6>
+        <p> Manual activation is required due to conflicts with search/sort functionalities,<br> so first make sure you're done editing those! </p>
+      `; // dont need references to any of these, so there's no reason to not use innerHTML. it halves the complexity & lines of code in this case
+
+      const toggleButtonElem = document.createElement("button");
+      toggleButtonElem.innerText = "ACTIVATE";
+      toggleSectionElem.appendChild(toggleButtonElem);
+
+      toggleButtonElem.addEventListener("click", function manualStartFiltering() {
+        startFiltering();
+        toggleButtonElem.removeEventListener("click", manualStartFiltering);
+        toggleSectionElem.remove();
+      });
+
+      containerElem.appendChild(toggleSectionElem);
     });
     initOnceReady.observe(
       document.body,
