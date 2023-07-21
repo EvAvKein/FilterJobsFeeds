50d0b98eb5f167e49253c13fc94a58eff7e31919
Eve Aviv Keinan <evavkein@gmail.com>
2023-07-21 16:03:06 +0300

    fix (package.sh): exclude git folder from zip
    i know i version-bumped when creating this file (and that the most important thing in versioning is being consistent), but imo this file is too peripheral to extension's functionality to be worth versioning

--- package.sh
+++ package.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 VERSION=$(grep -oP '(?<="version": ")[^"]*' manifest.json)
-zip -r ~/Desktop/filterJobsFeeds_$VERSION.zip .
+zip -r ~/Desktop/filterJobsFeeds_$VERSION.zip . -x .git/\*
